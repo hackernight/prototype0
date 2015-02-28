be98f8d023d74bfa12c3078eb8f36e0aa3183bfd
Zykenhouzen <peterzylka@gmail.com>
2015-02-28 15:02:47 -0600

    Added the base level scene. Complete with player and walls.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerControl : MonoBehaviour {
+	private Dictionary<KeyCode, Vector3> movementKeyMap = new Dictionary<KeyCode, Vector3>{
+		{KeyCode.W, Vector3.forward},
+		{KeyCode.A, Vector3.left},
+		{KeyCode.S, Vector3.back},
+		{KeyCode.D, Vector3.right},
+	};
+
+	public float speed = 2f;
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 deltaMove = Vector3.zero;
+		foreach(var key in movementKeyMap) {
+			if(Input.GetKey(key.Key)){
+				deltaMove += key.Value;
+			}
+		}
+
+		rigidbody.velocity = deltaMove * speed;
+	}
+	
+}
