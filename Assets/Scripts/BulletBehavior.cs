a914e178398397283b76734c08f7b6b9fe954737
Zykenhouzen <peterzylka@gmail.com>
2015-02-28 18:59:26 -0600

    This now has a loader!

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehavior : MonoBehaviour {
+	public Vector3 velocity = new Vector3(1f,0f,0f);
+	public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += velocity * speed;
+	}
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed{	
+	public class Loader : MonoBehaviour {
+		public GameObject gameController;
+		//public GameObject soundManager;/
+
+		void Awake (){
+
+				Instantiate (gameController);
+
+			
+			//if (gameController.instance == null) {
+				//Instantiate (soundManager);
+			//}
+		}
+	}
+}
